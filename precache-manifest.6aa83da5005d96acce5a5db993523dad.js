c7c4c79212c0813a2442bdcda234f379355d4606
Paul Fischer <paul.frank.fischer@gmail.com>
2020-02-19 17:05:33 +0100

    Updates

---
+++ precache-manifest.6aa83da5005d96acce5a5db993523dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13768d18df504aaa0bbab3b77b99708c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f870c4e1cd4565d7d4d",
+    "url": "/static/css/main.f5ddd33a.chunk.css"
+  },
+  {
+    "revision": "86b61964a70945cd188c",
+    "url": "/static/js/2.5570043e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5570043e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f870c4e1cd4565d7d4d",
+    "url": "/static/js/main.07cc13ea.chunk.js"
+  },
+  {
+    "revision": "f7c9aa25e720e7d6f3f6",
+    "url": "/static/js/runtime-main.4bd84f24.js"
+  }
+]);
